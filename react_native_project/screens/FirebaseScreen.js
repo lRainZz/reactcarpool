7285821b19460c00bc8a29f58344c4c23cd49228
Nicolas Heinen <heinenn@dhbw-loerrach.de>
2017-10-31 18:20:46 +0100

    Firebase implemented

--- react_native_project/screens/FirebaseScreen.js
+++ react_native_project/screens/FirebaseScreen.js
@@ -1,15 +1,34 @@
 // external modules
 
-import React from 'react';
+import React,  {Component} from 'react';
 
 import { Container } from 'native-base';
 
 import { Text } from 'react-native';
 
+import * as firebase from 'firebase';
+
 
 //own modules
 
 import Header from '../ApplicationHeader';
+
+
+
+//Firebase---------------------------------------------------------------------------------
+  //Initialize Firebase
+  const firebaseConfig  = {
+    apiKey: "AIzaSyDtR2LpBDSbEISKF_iv562aj7rYmBuXovA",
+    authDomain: "fahrgemeinschaft-22833.firebaseapp.com",
+    databaseURL: "https://fahrgemeinschaft-22833.firebaseio.com",
+    storageBucket: "fahrgemeinschaft-22833.appspot.com"
+  };
+  firebase.initializeApp(firebaseConfig );
+
+  // Create a reference with .ref() instead of new Firebase(url)
+  const rootRef = firebase.database().ref();
+  const itemsRef = rootRef.child('items');
+//-----------------------------------------------------------------------------------------
 
 
 // class
