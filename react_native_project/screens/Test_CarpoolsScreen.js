c590841f1a0cc81b6157dd43ab4fd6a3e15d4e69
Nicolas Heinen <Smitjo901@gmail.com>
2017-11-13 09:55:38 +0100

    Carpools-methods start

--- react_native_project/screens/Test_CarpoolsScreen.js
+++ react_native_project/screens/Test_CarpoolsScreen.js
@@ -17,7 +17,84 @@ import Header from '../ApplicationHeader';
 
 class FirebaseScreen extends React.Component { 
   
-  doSomething(value)
+  createNewCarpool()
+  {
+    try
+    {
+      alert('Test successfull!');
+    }catch(error)
+    {
+      
+    }
+  }
+
+  inviteToCarpool()
+  {
+    try
+    {
+      alert('Test successfull!');
+    }catch(error)
+    {
+      
+    }
+  }
+
+  joinCarpool()
+  {
+    try
+    {
+      alert('Test successfull!');
+    }catch(error)
+    {
+      
+    }
+  }
+
+  checkForInvite()
+  {
+    try
+    {
+      alert('Test successfull!');
+    }catch(error)
+    {
+      
+    }
+  }
+
+  CheckForJoin()
+  {
+    try
+    {
+      alert('Test successfull!');
+    }catch(error)
+    {
+      
+    }
+  }
+
+  acceptInvite()
+  {
+    try
+    {
+      alert('Test successfull!');
+    }catch(error)
+    {
+      
+    }
+  }
+
+  acceptJoin()
+  {
+    try
+    {
+      alert('Test successfull!');
+    }catch(error)
+    {
+      
+    }
+  }
+
+  deleteCarpool()
   {
     try
     {
@@ -35,10 +112,55 @@ class FirebaseScreen extends React.Component {
           onHeadButtonPress={() => this.props.navigation.navigate('DrawerToggle')}
           title='Test_Carpools'  
         />
+        <Text backgroundColor="red">
+          Red: Not implemented
+        </Text>
+        <Text backgroundColor="orange">
+          Orange: Work in progress
+        </Text>
+        <Text backgroundColor="green">
+          Green: Finished
+        </Text>
+
         <Button
-          onPress={this.doSomething.bind(this, 'test')}
-          title="Do something!"
-          color="green"
+          onPress={this.createNewCarpool.bind(this)}
+          title="Create new Carpool"
+          color="orange"
+        />
+        <Button
+          onPress={this.inviteToCarpool.bind(this)}
+          title="Invite someone to carpool"
+          color="red"
+        />
+        <Button
+          onPress={this.joinCarpool.bind(this)}
+          title="Ask to join Carpool"
+          color="red"
+        />
+        <Button
+          onPress={this.checkForInvite.bind(this)}
+          title="Check for invite"
+          color="red"
+        />
+        <Button
+          onPress={this.CheckForJoin.bind(this)}
+          title="Check for join intention"
+          color="red"
+        />
+        <Button
+          onPress={this.acceptInvite.bind(this)}
+          title="Accept invite"
+          color="red"
+        />
+        <Button
+          onPress={this.acceptJoin.bind(this)}
+          title="Accept join"
+          color="red"
+        />
+        <Button
+          onPress={this.deleteCarpool.bind(this)}
+          title="Delete Carpool"
+          color="red"
         />
       </Container>
     );
