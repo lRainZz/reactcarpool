97e0358ecd06d96cd154a0188c728964b6eb2d58
Nicolas Heinen <Smitjo901@gmail.com>
2017-12-24 00:38:44 +0100

    added checkForInvite in global

--- react_native_project/App.js
+++ react_native_project/App.js
@@ -167,6 +167,74 @@ class CarpoolApp extends React.Component {
                       GLOBALS.Users = {...GLOBALS.Users, ...JSONExport_Users};
                     })).then(
                       response => {
+
+                        let Export = [];
+                        let MaxCounter = 0;
+                        let Counter = 0;
+                        //Check if Carpool exists
+                        firebase.database().ref().child('UserCarpools').orderByChild('UserKey').equalTo(GLOBALS.UserKey).once('value')
+                        .then((snapshot) =>
+                        {
+                          Promise.all(snapshot.forEach((childSnapshot) => {
+                            if (childSnapshot.child('Invite').val() == '1'){
+                              MaxCounter++;
+                            }
+                          })).then(
+                            response =>
+                            {
+                              firebase.database().ref().child('UserCarpools').orderByChild('UserKey').equalTo(GLOBALS.UserKey).once('value')
+                              .then((snapshot) =>
+                              {
+                                snapshot.forEach((childSnapshot) => {
+                                  if (childSnapshot.child('Invite').val() == '1'){
+                                    CarpoolKey = snapshot.child(childSnapshot.key).child("CarpoolKey").val();
+                                    UserCarpoolKey = childSnapshot.key;
+                                    CurrentDate = snapshot.child(childSnapshot.key).child("Date").val();
+                                    //Get Creator
+                                    firebase.database().ref().child('UserCarpools').orderByChild('CarpoolKey').equalTo(CarpoolKey).once('value')
+                                    .then((snapshot2) =>
+                                    {
+                                      snapshot2.forEach((childSnapshot2) => {
+                                        if (childSnapshot2.child('Creator').val() == '1'){
+                                          CreatorKey = snapshot2.child(childSnapshot2.key).child("UserKey").val();
+                                          //Get Name of Creator
+                                          firebase.database().ref('/Users/' + CreatorKey).once('value')
+                                          .then((snapshot3) =>
+                                          {
+                                            let Fullname = snapshot3.val().FullName;
+                                            firebase.database().ref('/Carpools/' + CarpoolKey).once('value')
+                                            .then((snapshot4) =>
+                                            {
+                                              let CarpoolName = snapshot4.val().CarpoolName;
+                                              //let MaxPlace = snapshot4.val().MaxPlace;
+                                              //console.log(Fullname + ' from Carpool "' + CarpoolName + '" has invited you.');
+
+                                              let CarpoolObject = {};
+                                              CarpoolObject.CarpoolKey = CarpoolKey;
+                                              CarpoolObject.CarpoolName = CarpoolName;
+                                              CarpoolObject.Fullname = Fullname;
+                                              CarpoolObject.UserCarpoolKey = UserCarpoolKey;
+                                              CarpoolObject.Date = CurrentDate;
+
+                                              Counter++;
+                                              Export.push(CarpoolObject);
+                                              if(Counter == MaxCounter)
+                                              {
+                                                GLOBALS.Invites = Export;
+                                                console.log(GLOBALS.Invites)
+                                              }
+                                            });
+                                          });
+                                        }
+                                      });
+                                    });
+                                  }
+                                });
+                              });
+                            }
+                          );
+                        });
+
                         firebase.database().ref().child('UserCarpools').orderByChild('UserKey').equalTo(Userkey).once('value')
                         .then((CPCSnap) =>
                         {
